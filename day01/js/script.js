6ebd7a6dc4374925e9bd40f4d6026663789c15b5
Skye Reyes <Skye Reyes>
2017-05-15 20:56:55 -0700

    did some javascript stuff

---
+++ day01/js/script.js
@@ -0,0 +1,65 @@
+// ES6 - ECMAScript6
+
+// Good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscores;
+var $dollarsign;
+
+// Meh
+var SuperCamelCase;
+
+// Bad
+// var no spaces;
+// var no-hyphens;
+// var 1nope;
+
+console.log("sending a message to the console");
+console.info("sending an info message");
+console.warn("WARNING: UR GEY");
+console.error("o no");
+
+// console.log();
+
+// Single Line Comment
+
+/*
+  Multi
+  Line
+  Comment
+*/
+
+var
+
+
+
+                      freedom
+
+
+
+
+      =
+
+
+
+
+
+
+      "A string of javascript";
+
+
+console.log(freedom);
+
+// template string
+
+var javascripttemplatestring = `
+what
+ever
+i
+want
+:)`
+
+;
+
+console.log(javascripttemplatestring);
